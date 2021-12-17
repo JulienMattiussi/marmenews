40d3cf71accc40a4966865441a54286665be3e2d
Luwangel <adrien.amoros@gmail.com>
2021-12-17 14:46:42 +0100

    Fix importing props from next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps } from "next";
+
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
