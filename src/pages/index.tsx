743824a01f39d2aeda259077110606fb6cb82211
JulienM <julienm@marmelab.com>
2022-03-18 16:11:35 +0100

    use next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from "next";
+import { GetStaticProps, NextPageContext } from "next";
+import { useSession, getSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 import Layout from "../components/Layout";
@@ -21,83 +22,106 @@ type Props = {
     current: number;
     pages: number;
   };
+  session: any;
 };
 
 export default function Index({ posts, tags, pagination }: Props) {
+  const { data: session } = useSession();
   const url = "/";
-  return (
-    <Layout>
-      <BasicMeta url={url} />
-      <OpenGraphMeta url={url} />
-      <TwitterCardMeta url={url} />
-      <div className="container">
-        <div>
-          <header>
-            <h1>La Marmezette</h1>
-            <span className="handle">Par Karen</span>
-          </header>
-          <section>
-            <h2>Dernières Nouvelles</h2>
-            <PostList posts={posts} tags={tags} pagination={pagination} />
-          </section>
+  console.log("session");
+  console.log(session);
+  if (session) {
+    return (
+      <Layout>
+        <BasicMeta url={url} />
+        <OpenGraphMeta url={url} />
+        <TwitterCardMeta url={url} />
+        <div className="container">
+          <div>
+            <header>
+              <h1>La Marmezette</h1>
+              <span className="handle">Par Karen</span>
+            </header>
+            <section>
+              <h2>Dernières Nouvelles</h2>
+              <PostList posts={posts} tags={tags} pagination={pagination} />
+            </section>
+          </div>
         </div>
-      </div>
-      <div className="aside">
-        <h5>Liens utiles</h5>
-        <ExternalLinks />
-        <h5>Categories</h5>
-        <PostCategoriesList tags={tags} />
-        <h5>Réseaux "Sociaux"</h5>
-        <SocialList />
-      </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-          max-width: 900px;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.5rem;
-          font-weight: 400;
-          line-height: 1.25;
-          text-decoration: underline;
-          text-underline-offset: 8px;
-        }
-        h5 {
-          margin: 16px 0px 8px;
-        }
-
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
-
-        @media (min-width: 769px) {
+        <div className="aside">
+          <h5>Liens utiles</h5>
+          <ExternalLinks />
+          <h5>Categories</h5>
+          <PostCategoriesList tags={tags} />
+          <h5>Réseaux "Sociaux"</h5>
+          <SocialList />
+        </div>
+        <style jsx>{`
+          .container {
+            display: flex;
+            flex-direction: column;
+            flex: 1 1 auto;
+            padding: 0 1.5rem;
+            max-width: 900px;
+          }
           h1 {
-            font-size: 3rem;
+            font-size: 2.5rem;
+            margin: 0;
+            font-weight: 500;
           }
           h2 {
-            font-size: 2rem;
+            font-size: 1.5rem;
+            font-weight: 400;
+            line-height: 1.25;
+            text-decoration: underline;
+            text-underline-offset: 8px;
           }
-        }
-      `}</style>
-    </Layout>
+          h5 {
+            margin: 16px 0px 8px;
+          }
+
+          .fancy {
+            color: #15847d;
+          }
+          .handle {
+            display: inline-block;
+            margin-top: 0.275em;
+            color: #9b9b9b;
+            letter-spacing: 0.05em;
+          }
+
+          @media (min-width: 769px) {
+            h1 {
+              font-size: 3rem;
+            }
+            h2 {
+              font-size: 2rem;
+            }
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+  return (
+    <>
+      <span>Not signed in </span>
+      <br />
+      <a
+        href={`/api/auth/signin`}
+        onClick={(e) => {
+          e.preventDefault();
+          signIn();
+        }}
+      >
+        Sign in
+      </a>
+    </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+/* export const getStaticProps: GetStaticProps = async (
+  context: NextPageContext
+) => {
   const posts = listPostContent(1, config.posts_per_page);
   const tags = listTags();
   const pagination = {
@@ -109,6 +133,16 @@ export const getStaticProps: GetStaticProps = async () => {
       posts,
       tags,
       pagination,
+      session: await getSession(context),
     },
   };
-};
+}; */
+
+// Export the `session` prop to use sessions with Server Side Rendering
+export async function getServerSideProps(context: NextPageContext) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
+}
